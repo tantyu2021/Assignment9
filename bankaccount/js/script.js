5d0f47b057f91dd922f6b3ffbae80487b367a6d4
tantyu2021 <tantyu2021@gmail.com>
2020-05-01 22:55:51 -0700

    bankaccount

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -2,12 +2,14 @@
 
 //===Part 1 – The UI=====================
 var bankprocess = function() {
-    var name, deposit, withdrawal, msgN, msgD, msgW, btitle;
+    var name, deposit, withdrawal, msgN, msgD, msgW, btitle, filling;
 
     //msg = 'Please complete all required info.';
+    filling = '';
     msgN = 'Please enter your name.';
     msgD = 'Please enter your deposit.';
     msgW = 'Please enter your withdrawal.';
+    btitle = 'Table-info. Requared fields.';
     //name = document.getElementById('name').value;
     name = prompt('Enter your name'); 
     //deposit = document.getElementById('deposit').value;
@@ -33,14 +35,15 @@ document.getElementById('nReg').innerHTML = 'Name: ' + name + '<br>';
 document.getElementById('dReg').innerHTML = 'Deposit: ' + deposit + '<br>';
 document.getElementById('wReg').innerHTML = 'Withdrawal: ' + withdrawal + '<br>';
 
-    // //when writing red
-    // document.getElementById('bankregister_title').innerHTML = btitle;
-    // if (btitle === msg) {
-    //     html += '<tr><td>Name:</td><td>' + name + '</td></tr>';
-    //     html += '<tr><td>Deposit:</td><td>' + dewposit + '</td></tr>';
-    //     html += '<tr><td>Withdrawal:</td><td>' + withdrawal + '</td></tr>';
+//fromlab13 -t15
+
+    document.getElementById('bankregister_title').innerHTML = btitle;
+        filling += '<tr><td>Name:</td><td>' + name + '</td></tr>';
+        filling += '<tr><td>Deposit:</td><td>' + deposit + '</td></tr>';
+        filling += '<tr><td>Withdrawal:</td><td>' + withdrawal + '</td></tr>';
+        document.getElementById('registration_info').innerHTML = filling;
         
-    // } 
+
 };
 
 window.addEventListener('load', function() {
@@ -58,9 +61,39 @@ window.addEventListener('load', function() {
 
 
 //===Part 2 – The Closure Function================
-var ownerName = prompt('Please enter your name')
-function bankAccount (ownerName) {
-    var balance, owner;
-     withdrawal(withdrawalAmount);
-}
 
+// function bankAccount (ownerName) {
+//     var balance, owner;
+//      withdrawal(withdrawalAmount);
+// }
+
+function bankAccount(amount) {
+    this.balance = amount; 
+    this.deposit = deposit; 
+    this.withdraw = withdraw; 
+    this.toString = toString; 
+} 
+function deposit(amount) { 
+    this.balance += amount; 
+} 
+function withdraw(amount) { 
+    if (amount <= this.balance) { 
+        this.balance -= amount; 
+    } 
+    if (amount > this.balance) { 
+        print("cannot process"); 
+    } 
+} 
+function toString() { 
+    return "Balance: " + this.balance; 
+} 
+var account = new bankAccount(150); 
+account.deposit(100); 
+console.log(account.toString()); 
+//window.document.write("Balance: " + this.balance);
+account.withdraw(70); 
+console.log(account.toString());
+//window.document.write("Balance: " + this.balance); 
+account.withdraw(8);    
+console.log(account.toString());
+//window.document.write("Balance: " + this.balance);
